53e7e726977b369cb0d3615e0df736fce2aa3188
alentandara <alen2211@gmail.com>
2026-01-27 18:50:30 +0100

    uvod u java skript

---
+++ 2_JS/vjezba01/skripta.js
@@ -0,0 +1,9 @@
+console.info('Hello world 5')
+console.table(JSON.parse('{"name":"John", "age":30, "car":null}'));
+console.log("O'Neal"); //\atlgr+q
+
+console.log(`Hello 
+    World
+    6`); // 'ALTGR+7
+
+    document.getElementById('ispis').innerHTML='<a href="#">Poveznica</a>';
