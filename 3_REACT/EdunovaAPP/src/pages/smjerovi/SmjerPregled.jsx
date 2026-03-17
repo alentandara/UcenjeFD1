0c5afc7cc40e61ddcca5a8710805b170124a02ba
alentandara <alen2211@gmail.com>
2026-03-17 18:53:22 +0100

    smjerovi

--- 3_REACT/EdunovaAPP/src/pages/smjerovi/SmjerPregled.jsx
+++ 3_REACT/EdunovaAPP/src/pages/smjerovi/SmjerPregled.jsx
@@ -1,7 +1,32 @@
+import { useEffect, useState } from "react"
+import SmjerService from "../../services/smjerovi/SmjerService"
+
 export default function SmjerPregled(){
+
+     const [smjerovi, setSmjerovi] = useState([])
+
+     useEffect(()=>{
+
+     
+     },[])
+
+     async function ucitajSmjerove {
+        await SmjerService.got().then((odgovor)=>{
+
+            setSmjerovi(odgovor.data)
+        })
+        
+     }
+
+
+
     return(
 <>
-Ovdje će se vidjeti smjerovi
+<ul>
+    {smjerovi && smjerovi.map((smjer)=>(
+        <li>{smjer.naziv}</li>
+    ))}
+</ul>
 </>
 
     )
---
+++ 3_REACT/EdunovaAPP/src/services/smjerovi/SmjerPodaci.js
@@ -0,0 +1,20 @@
+export constsmjerovi = [
+    {
+        sifra: 1,
+        naziv: 'Web programiranje'
+        trajanje: 130,
+        cijena: 1250.99,
+        datumPokretanja: '2024-02-21t17:00:00',
+        aktivan: true
+    },
+
+    {
+       sifra: ,
+        naziv: 'Java programiranje'
+        trajanje: 130,
+        cijena: 1500,
+        datumPokretanja: '2019-02-21t17:00:00',
+        aktivan: false
+    }
+]
+
